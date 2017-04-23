32fc991b66a6400e3af31eff3dccdaa2de83206f
AronLowe <lowe.aron@gmail.com>
2017-04-23 09:52:47 -0500

    initial todo commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      task1: 'mow lawn',
+      task2: 'clean room',
+      task3: 'pay bills'
+    }
+  });
+});
